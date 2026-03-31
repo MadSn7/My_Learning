5b2f33e5adf30f345f3974dd6516837474a99dc0
MadSn7 <nagar.madhur17@gmail.com>
2026-04-01 01:34:16 +0530

    Add *.class to .gitignore

---
+++ CSES_Solutions_java/src/main/java/org/example/A1_Introductory_Problems/C3Repetitions.java
@@ -0,0 +1,4 @@
+package org.example.A1_Introductory_Problems;
+
+public class C3Repetitions {
+}
---
+++ CSES_Solutions_java/src/main/java/org/example/A1_Introductory_Problems/D4IncreasingArray.java
@@ -0,0 +1,32 @@
+//package org.example.A1_Introductory_Problems;
+
+import java.util.Scanner;
+
+public class C4IncreasingArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long ans = 0;
+
+//        int prev = sc.nextInt();
+//        for(int i = 1; i < n; i++){
+//            int curr = sc.nextInt();
+//            ans += Math.max(0, prev - curr);
+//            prev = curr;
+//        }
+//        System.out.println(prev);
+        int prevMax = sc.nextInt();
+        for (int i = 1; i < n; i++) {
+            int curr =  sc.nextInt();
+            if (curr >= prevMax) {
+                prevMax = curr;
+            }else{
+                ans += prevMax - curr;
+            }
+//            ans += Math.max(0, prevMax - curr);
+//            prevMax = Math.max(prevMax, curr);
+        }
+        System.out.println(ans);
+        sc.close();
+    }
+}
